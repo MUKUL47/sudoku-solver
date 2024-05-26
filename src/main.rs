bedca75ac4f62cc66f4670916096155dfc80ebbb
Mukul Dutt <mukuldutt1234@gmail.com>
2024-05-26 02:47:02 +0200

    added visualizer+

--- src/main.rs
+++ src/main.rs
@@ -1,22 +1,94 @@
 mod sudoku_solver;
-fn main() {
+use macroquad::{experimental::coroutines::wait_seconds, prelude::*, window};
+use sudoku_solver::*;
+#[macroquad::main("Sudoku Solver")]
+async fn main() {
     let grid = [
-        [0, 1, 0, 2, 0, 0, 0, 0, 3],
-        [0, 0, 4, 0, 0, 0, 0, 0, 5],
-        [0, 6, 0, 0, 1, 0, 7, 0, 0],
-        [4, 0, 0, 0, 0, 0, 0, 8, 0],
-        [0, 0, 0, 5, 0, 9, 0, 0, 0],
-        [0, 3, 0, 0, 0, 0, 0, 0, 2],
-        [0, 0, 2, 0, 6, 0, 0, 1, 0],
-        [7, 0, 0, 0, 0, 0, 9, 0, 0],
-        [5, 0, 0, 0, 0, 8, 0, 4, 0],
+        [2, 0, 0, 5, 0, 3, 0, 0, 0],
+        [8, 0, 7, 0, 2, 0, 0, 0, 0],
+        [0, 0, 0, 0, 7, 0, 0, 0, 6],
+        [0, 0, 5, 2, 0, 7, 6, 1, 0],
+        [0, 2, 1, 6, 0, 5, 3, 0, 7],
+        [3, 7, 0, 0, 0, 8, 0, 0, 0],
+        [0, 0, 8, 0, 0, 0, 0, 4, 0],
+        [6, 0, 2, 4, 8, 0, 0, 0, 0],
+        [0, 9, 0, 7, 5, 2, 8, 6, 0],
     ];
-    match sudoku_solver::solve(&grid) {
-        None => {
-            println!("NO SOLUTION FOUND!");
+    let mut counter: f32 = 1.0;
+    let speed = 1.;
+    let mut sudoku_solver = SudokuSolver::new(&grid);
+    // while sudoku_solver.current_state == GAME_STATE::InProgress {  without visualization
+    //     sudoku_solver.iterate();
+    // }
+    loop {
+        clear_background(WHITE);
+        let dimension_x = screen_width() / 9.0;
+        let dimension_y = screen_height() / 9.0;
+        if counter % speed == 0. {
+            sudoku_solver.iterate();
         }
-        Some(g) => {
-            println!("{:?}", g);
+        for i in 0..9 {
+            for j in 0..9 {
+                let is_active = is_active_coord(&sudoku_solver, j, i);
+                let is_completed_coord = sudoku_solver.is_completed_coord(j, i);
+                let cell_done = sudoku_solver.grid[j as usize][i as usize] != 0;
+                if is_active || is_completed_coord || cell_done {
+                    let c = if is_active {
+                        YELLOW
+                    }else if is_completed_coord {
+                        PINK
+                    }else {
+                        BLUE
+                    };
+                    draw_rectangle(
+                        dimension_x * i as f32,
+                        dimension_y * j as f32,
+                        dimension_x,
+                        dimension_y,
+                        c,
+                    )
+                }
+                draw_rectangle_lines(
+                    dimension_x * i as f32,
+                    dimension_y * j as f32,
+                    dimension_x,
+                    dimension_y,
+                    1.0,
+                    BLACK,
+                );
+                let (x, y) = get_dim(is_active);
+                draw_text(
+                    &i32::to_string(&sudoku_solver.grid[j as usize][i as usize]),
+                    dimension_x / x + (dimension_x * i as f32),
+                    dimension_y / y + (dimension_y * j as f32),
+                    get_font_size(is_active),
+                    get_coord_color(is_active),
+                );
+            }
         }
+        counter += 1.;
+        next_frame().await;
     }
+}
+
+fn get_coord_color(is_active: bool) -> Color {
+    if is_active {
+        return RED;
+    }
+    BLACK
+}
+fn get_font_size(is_active: bool) -> f32 {
+    if is_active {
+        return 50.;
+    }
+    30.
+}
+fn get_dim(is_active: bool) -> (f32, f32) {
+    if is_active {
+        return (2.5, 1.5);
+    }
+    (2.25, 1.75)
+}
+fn is_active_coord(solver: &SudokuSolver, i: i32, j: i32) -> bool {
+    solver.current_grid_index.0 == i && solver.current_grid_index.1 == j
 }
--- src/sudoku_solver/mod.rs
+++ src/sudoku_solver/mod.rs
@@ -1,190 +1,227 @@
+use rand::*;
 use std::collections::{HashMap, HashSet};
-static ROW_COL_SUM: i32 = 45;
 type Grid = [[i32; 9]; 9];
-pub fn solve(g: &Grid) -> Option<Grid>{
-    let mut grid = g.clone();
-    let mut completed_cell_coord: HashSet<i8> = HashSet::new();
-    let mut visited: HashMap<i8, HashSet<i32>> = HashMap::new();
-    let mut current_grid_index: (i32, i32) = (0, 0);
-    for i in 0..9 {
-        for j in 0..9 {
-            let h = hash_coord(&i, &j);
-            visited.insert(h.clone(), HashSet::new());
-            if grid[i as usize][j as usize] != 0 {
-                completed_cell_coord.insert(h);
+#[derive(PartialEq)]
+pub enum GAME_STATE {
+    InProgress,
+    Completed,
+    Failed,
+}
+pub struct SudokuSolver {
+    pub grid: Grid,
+    pub completed_cell_coord: HashSet<i8>,
+    pub visited: HashMap<i8, HashSet<i32>>,
+    pub current_grid_index: (i32, i32),
+    pub is_backtrack: bool,
+    pub current_state: GAME_STATE,
+}
+
+impl SudokuSolver {
+    pub fn new(g: &Grid) -> Self {
+        let mut solver = SudokuSolver {
+            completed_cell_coord: HashSet::new(),
+            visited: HashMap::new(),
+            current_grid_index: (0, 0),
+            grid: g.clone(),
+            is_backtrack: false,
+            current_state: GAME_STATE::InProgress,
+        };
+        for i in 0..9 {
+            for j in 0..9 {
+                let h = SudokuSolver::hash_coord(&i, &j);
+                solver.visited.insert(h.clone(), HashSet::new());
+                if solver.grid[i as usize][j as usize] != 0 {
+                    solver.completed_cell_coord.insert(h);
+                }
             }
         }
+        solver
     }
-    let mut is_backtrack = false;
-    loop {
-        let c_i = current_grid_index.0;
-        let c_j = current_grid_index.1;
-        let coord_hash = hash_coord(&c_i, &c_j);
-        if !is_backtrack {
+
+    pub fn is_completed_coord(&self, i: i32, j: i32) -> bool {
+        return self
+            .completed_cell_coord
+            .contains(&SudokuSolver::hash_coord(&i, &j));
+    }
+
+    pub fn iterate(&mut self) -> &mut Self {
+        if self.current_state != GAME_STATE::InProgress {
+            return self;
+        }
+        let c_i = self.current_grid_index.0;
+        let c_j = self.current_grid_index.1;
+        let coord_hash = SudokuSolver::hash_coord(&c_i, &c_j);
+        if !self.is_backtrack {
             if c_i > 8 {
-                return Some(grid);              
+                self.current_state = GAME_STATE::Completed;
+                return self;
             }
-            if completed_cell_coord.contains(&coord_hash) {
-                current_grid_index = get_next_index(c_i, c_j);
-                continue;
+            if self.completed_cell_coord.contains(&coord_hash) {
+                self.current_grid_index = SudokuSolver::get_next_index(c_i, c_j);
+                return self;
             }
-            let current_coord_set: &mut HashSet<i32> = visited.get_mut(&coord_hash).unwrap();
-            match get_unvisited_coord(current_coord_set, c_i, c_j, &grid) {
+            let current_coord_set: &mut HashSet<i32> = self.visited.get_mut(&coord_hash).unwrap();
+            match SudokuSolver::get_unvisited_coord(current_coord_set, c_i, c_j, &self.grid) {
                 None => {
-                    is_backtrack = true;
+                    self.is_backtrack = true;
                 }
                 Some(coordinate) => {
                     current_coord_set.insert(coordinate);
-                    grid[c_i as usize][c_j as usize] = coordinate;
-                    current_grid_index = get_next_index(c_i, c_j);
+                    self.grid[c_i as usize][c_j as usize] = coordinate;
+                    self.current_grid_index = SudokuSolver::get_next_index(c_i, c_j);
                 }
             }
-            continue;
+            return self;
         }
-        visited.insert(coord_hash, HashSet::new());
-        grid[c_i as usize][c_j as usize] = 0;
+        self.visited.insert(coord_hash, HashSet::new());
+        self.grid[c_i as usize][c_j as usize] = 0;
         if c_i == 0 && c_j == 0 {
-            return None;                
+            self.current_state = GAME_STATE::Failed;
+            return self;
         }
-        let mut tmp_curr = get_previous_index(c_i, c_j);
+        let mut tmp_curr = SudokuSolver::get_previous_index(c_i, c_j);
         loop {
-            let h = hash_coord(&tmp_curr.0, &tmp_curr.1);
-            if !completed_cell_coord.contains(&h) {
+            let h = SudokuSolver::hash_coord(&tmp_curr.0, &tmp_curr.1);
+            if !self.completed_cell_coord.contains(&h) {
                 break;
             }
-            tmp_curr = get_previous_index(tmp_curr.0, tmp_curr.1);
+            tmp_curr = SudokuSolver::get_previous_index(tmp_curr.0, tmp_curr.1);
         }
-        current_grid_index = tmp_curr;
-        is_backtrack = false;
+        self.current_grid_index = tmp_curr;
+        self.is_backtrack = false;
+        return self;
     }
-}
 
-fn get_unvisited_coord(set: &HashSet<i32>, i: i32, j: i32, grid: &Grid) -> Option<i32> {
-    //
-    let mut current_visisted: HashSet<i32> = HashSet::new();
-    while current_visisted.len() + set.len() < 9 {
-        for n in 1..=9 {
+    fn hash_coord(i: &i32, j: &i32) -> i8 {
+        return (i * 10 + j) as i8;
+    }
+
+    fn get_unvisited_coord(set: &HashSet<i32>, i: i32, j: i32, grid: &Grid) -> Option<i32> {
+        //
+        let mut current_visisted: HashSet<i32> = HashSet::new();
+        while current_visisted.len() + set.len() < 9 {
+            let mut rng = rand::thread_rng();
+            let n: i32 = rng.gen_range(1..=9);
+            // for n in 1..=9 {
             if !set.contains(&n) && !current_visisted.contains(&n) {
                 let mut g = grid.clone();
                 g[i as usize][j as usize] = n;
-                if is_valid_cell(i, j, &g) {
+                if SudokuSolver::is_valid_cell(i, j, &g) {
                     return Some(n);
                 }
                 current_visisted.insert(n);
             }
+            // }
         }
+        return None;
     }
-    return None;
-}
 
-fn get_previous_index(i: i32, j: i32) -> (i32, i32) {
-    if j == 0 {
-        (i - 1, 8)
-    } else {
-        (i, j - 1)
-    }
-}
-
-fn get_next_index(i: i32, j: i32) -> (i32, i32) {
-    if (j + 1) % 9 == 0 {
-        (i + 1, 0)
-    } else {
-        (i, (j + 1) % 9)
-    }
-}
-
-fn is_board_valid(grid: &Grid) -> bool {
-    for i in 0..9 {
-        for j in 0..9 {
-            if grid[i][j] == 0 || !is_valid_cell(i as i32, j as i32, grid) {
-                return false;
-            }
-        }
-    }
-    return true;
-}
-
-fn is_valid_cell(i: i32, j: i32, grid: &Grid) -> bool {
-    let mut x_sum: HashSet<i32> = HashSet::new();
-    let mut y_sum: HashSet<i32> = HashSet::new();
-    for x in 0..9 {
-        let a = grid[x][j as usize];
-        let b = grid[i as usize][x];
-        if a != 0 {
-            if x_sum.contains(&a) {
-                return false;
-            }
-            x_sum.insert(a);
-        }
-        if b != 0 {
-            if y_sum.contains(&b) {
-                return false;
-            }
-            y_sum.insert(b);
-        }
-    }
-    let ([xi, xj], [yi, yj]) = get_segment_coordinates(get_segment(i, j));
-    let mut visited: HashSet<i32> = HashSet::new();
-    for i in yi..=yj {
-        for j in xi..=xj {
-            let c = grid[i as usize][j as usize];
-            if visited.contains(&c) && c > 0 {
-                return false;
-            }
-            visited.insert(c);
-        }
-    }
-    return true;
-}
-
-fn get_segment_coordinates(index: i32) -> ([i32; 2], [i32; 2]) {
-    match index {
-        0 => ([0, 2], [0, 2]),
-        1 => ([0, 2], [3, 5]),
-        2 => ([0, 2], [6, 8]),
-        3 => ([3, 5], [0, 2]),
-        4 => ([3, 5], [3, 5]),
-        5 => ([3, 5], [6, 8]),
-        6 => ([6, 8], [0, 2]),
-        7 => ([6, 8], [3, 5]),
-        8 => ([6, 8], [6, 8]),
-        _ => panic!("get_segment_coordinates - Invalid index"),
-    }
-}
-
-fn get_segment(i: i32, j: i32) -> i32 {
-    // Top
-    if j < 3 {
-        if i < 3 {
-            return 0;
-        } else if i < 6 {
-            return 1;
+    fn get_previous_index(i: i32, j: i32) -> (i32, i32) {
+        if j == 0 {
+            (i - 1, 8)
         } else {
-            return 2;
+            (i, j - 1)
         }
     }
-    // Mid
-    else if j < 6 {
-        if i < 3 {
-            return 3;
-        } else if i < 6 {
-            return 4;
-        } else {
-            return 5;
-        }
-    }
-    // Bottom
-    else {
-        if i < 3 {
-            return 6;
-        } else if i < 6 {
-            return 7;
-        } else {
-            return 8;
-        }
-    }
-}
 
-fn hash_coord(i: &i32, j: &i32) -> i8 {
-    return (i * 10 + j) as i8;
+    fn get_next_index(i: i32, j: i32) -> (i32, i32) {
+        if (j + 1) % 9 == 0 {
+            (i + 1, 0)
+        } else {
+            (i, (j + 1) % 9)
+        }
+    }
+
+    fn is_board_valid(grid: &Grid) -> bool {
+        for i in 0..9 {
+            for j in 0..9 {
+                if grid[i][j] == 0 || !SudokuSolver::is_valid_cell(i as i32, j as i32, grid) {
+                    return false;
+                }
+            }
+        }
+        return true;
+    }
+
+    fn is_valid_cell(i: i32, j: i32, grid: &Grid) -> bool {
+        let mut x_sum: HashSet<i32> = HashSet::new();
+        let mut y_sum: HashSet<i32> = HashSet::new();
+        for x in 0..9 {
+            let a = grid[x][j as usize];
+            let b = grid[i as usize][x];
+            if a != 0 {
+                if x_sum.contains(&a) {
+                    return false;
+                }
+                x_sum.insert(a);
+            }
+            if b != 0 {
+                if y_sum.contains(&b) {
+                    return false;
+                }
+                y_sum.insert(b);
+            }
+        }
+        let ([xi, xj], [yi, yj]) =
+            SudokuSolver::get_segment_coordinates(SudokuSolver::get_segment(i, j));
+        let mut visited: HashSet<i32> = HashSet::new();
+        for i in yi..=yj {
+            for j in xi..=xj {
+                let c = grid[i as usize][j as usize];
+                if visited.contains(&c) && c > 0 {
+                    return false;
+                }
+                visited.insert(c);
+            }
+        }
+        return true;
+    }
+
+    fn get_segment_coordinates(index: i32) -> ([i32; 2], [i32; 2]) {
+        match index {
+            0 => ([0, 2], [0, 2]),
+            1 => ([0, 2], [3, 5]),
+            2 => ([0, 2], [6, 8]),
+            3 => ([3, 5], [0, 2]),
+            4 => ([3, 5], [3, 5]),
+            5 => ([3, 5], [6, 8]),
+            6 => ([6, 8], [0, 2]),
+            7 => ([6, 8], [3, 5]),
+            8 => ([6, 8], [6, 8]),
+            _ => panic!("get_segment_coordinates - Invalid index"),
+        }
+    }
+
+    fn get_segment(i: i32, j: i32) -> i32 {
+        // Top
+        if j < 3 {
+            if i < 3 {
+                return 0;
+            } else if i < 6 {
+                return 1;
+            } else {
+                return 2;
+            }
+        }
+        // Mid
+        else if j < 6 {
+            if i < 3 {
+                return 3;
+            } else if i < 6 {
+                return 4;
+            } else {
+                return 5;
+            }
+        }
+        // Bottom
+        else {
+            if i < 3 {
+                return 6;
+            } else if i < 6 {
+                return 7;
+            } else {
+                return 8;
+            }
+        }
+    }
 }
